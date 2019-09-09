8890805755bfec035b0238ea206fa6bf0d2ab52f
maeganskinnerblog <maeganskinnerblog@gmail.com>
2019-09-09 09:15:47 -0500

    editedCangees2

--- Part1/index.js
+++ Part1/index.js
@@ -14,7 +14,7 @@ function play(clickedId) {
     board[clickedId] = 'O';
   }
   console.log(board);
-}
+
   const topLeft = board[0];
   const topCenter = board[1];
   const topRight = board[2];
@@ -26,44 +26,44 @@ function play(clickedId) {
   const bottomRight = board[8];
 
   if (topLeft !== undefined && topLeft === topCenter && topLeft === topRight) {
-  alert(`${topLeft} is the winner`);
-  return;
+    alert(`${topLeft} is the winner`);
+    return;
   }
   if (middleLeft !== undefined && middleLeft === middleCenter && middleLeft === middleRight) {
-  alert(`${middleLeft} is the winner`);
-  return;
+    alert(`${middleLeft} is the winner`);
+    return;
   }
   if (bottomLeft !== undefined && bottomLeft === bottomCenter && bottomLeft === bottomRight) {
-  alert(`${bottomLeft} is the winner`);
-  return;
+    alert(`${bottomLeft} is the winner`);
+    return;
   }
   if (topLeft !== undefined && topLeft === middleLeft && topLeft === bottomLeft) {
-  alert(`${topLeft} is the winner`);
-  return;
+    alert(`${topLeft} is the winner`);
+    return;
   }
   if (topCenter !== undefined && topCenter === middleCenter && topCenter === bottomCenter) {
-  alert(`${topCenter} is the winner`);
-  return;
+    alert(`${topCenter} is the winner`);
+    return;
   }
   if (topRight !== undefined && topRight === middleRight && topRight === bottomRight) {
-  alert(`${topRight} is the winner`);
-  return;
+    alert(`${topRight} is the winner`);
+    return;
   }
   if (topLeft !== undefined && topLeft === middleCenter && topLeft === bottomRight) {
-  alert(`${topLeft} is the winner`);
-  return;
+    alert(`${topLeft} is the winner`);
+    return;
   }
   if (bottomLeft !== undefined && bottomLeft === middleCenter && bottomLeft === topRight) {
-  alert(`${bottomLeft} is the winner`);
-  return;
+    alert(`${bottomLeft} is the winner`);
+    return;
   }
 
-  let boardFull = true;
-    for (let i = 0; i <= 8; i++) {
-      if (board[i] === undefined) {
-        boardFull = false;
-      }
+  let board = true;
+  for (let i = 0; i <= 8; i++) {
+    if (board[i] === undefined) {
+      boardFull = false;
     }
+  }
   if (boardFull === true) {
     alert("No winner");
   }
